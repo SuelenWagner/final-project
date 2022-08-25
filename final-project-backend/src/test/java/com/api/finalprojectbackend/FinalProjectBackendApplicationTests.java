82de14b2a6884c8e9844648728d6ab5b7b1c28db
SuelenWagner <siwc94@gmail.com>
2022-08-24 22:04:37 -0300

    Project created

---
+++ final-project-backend/src/test/java/com/api/finalprojectbackend/FinalProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.finalprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
